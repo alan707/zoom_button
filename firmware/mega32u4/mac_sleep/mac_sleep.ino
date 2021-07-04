21d13535acd96c29e2b7cc5135de9b4bc98baca2
Alan Mond <alan.mond@gmail.com>
2021-07-03 22:20:36 -0700

    Adds Mac Sleep Program

---
+++ firmware/mega32u4/mac_sleep/mac_sleep.ino
@@ -0,0 +1,76 @@
+
+
+/*
+ * Project Mac Sleep 😴
+ * Description: Smash the button to put your Mac to sleep
+ * Author: Alan Mond
+ * Date: 3 July 2021
+ * License: GNU GPLv3 
+ */
+
+#include <Keyboard.h>
+#include "LowPassFilter.hpp"
+
+#define NORMAL // define DEBUG for development or NORMAL for production
+
+constexpr int threshold = 1023 / 2; // set threshold as a constant expression, calculated at compile time.
+int raw_value = 0;
+int filtered_value = 0;
+bool is_button_pressed = false;
+bool keystroke_sent = false;
+LowPassFilter lpf(1, 0.007);
+
+void check_state_and_send_shortcut(){
+  if (is_button_pressed && !keystroke_sent){
+    // send escape key to access VIM command line
+    Keyboard.press(KEY_LEFT_GUI);
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.write('q');
+    Keyboard.releaseAll();
+    // press and release enter key to execute VIM command
+    // Keyboard.write(KEY_RETURN);
+    keystroke_sent = true;
+  }
+  Keyboard.end();
+}
+ 
+void turn_on_light(){
+  digitalWrite(13, HIGH);
+}
+
+void turn_off_light(){
+  digitalWrite(13, LOW);
+}
+
+void setup() { 
+  #ifdef DEBUG
+    Serial.begin(115200);
+  #endif
+  pinMode(13, OUTPUT);
+  pinMode(A0, INPUT_PULLUP);
+  Keyboard.begin();
+}
+
+void loop() {
+  raw_value = analogRead(A0);
+  filtered_value = lpf.update(raw_value);
+  #ifdef DEBUG
+    Serial.print("raw value is: ");
+    Serial.print(raw_value);
+    Serial.print(" | filtered value is: ");
+    Serial.print(filtered_value);
+    Serial.println();
+  #endif
+  if ( filtered_value < threshold) {
+    is_button_pressed = true;
+    turn_on_light();
+  }
+  else {
+    is_button_pressed = false;
+    keystroke_sent = false;
+    turn_off_light();
+  }
+  #ifdef NORMAL
+    check_state_and_send_shortcut();
+  #endif
+}
