b7deca58430552de5426a55a8ae915597e9e1ca9
Alan Mond <alan.mond@gmail.com>
2021-07-03 21:57:14 -0700

    Add Quit Vim Program

---
+++ firmware/mega32u4/quit_vim/LowPassFilter.cpp
@@ -0,0 +1,50 @@
+
+#include "LowPassFilter.hpp"
+#include <math.h>
+
+#define ERROR_CHECK (false)
+
+#if ERROR_CHECK
+#include <iostream>
+#endif
+
+LowPassFilter::LowPassFilter():
+  output(0),
+  ePow(0){}
+
+LowPassFilter::LowPassFilter(float iCutOffFrequency, float iDeltaTime):
+  output(0),
+  ePow(1-exp(-iDeltaTime * 2 * PI * iCutOffFrequency))
+{
+  #if ERROR_CHECK
+  if (iDeltaTime <= 0){
+    std::cout << "Warning: A LowPassFilter instance has been configured with 0 s as delta time.";
+    ePow = 0;
+  }
+  if(iCutOffFrequency <= 0){
+    std::cout << "Warning: A LowPassFilter instance has been configured with 0 Hz as cut-off frequency.";
+    ePow = 0;
+  }
+  #endif
+}
+
+float LowPassFilter::update(float input){
+  return output += (input - output) * ePow;
+}
+
+float LowPassFilter::update(float input, float deltaTime, float cutoffFrequency){
+  reconfigureFilter(deltaTime, cutoffFrequency); //Changes ePow accordingly.
+  return output += (input - output) * ePow;
+}
+
+void LowPassFilter::reconfigureFilter(float deltaTime, float cutoffFrequency){
+  #if ERROR_CHECK
+  if (deltaTime <= 0){
+    ePow = 0;
+  }
+  if(cutoffFrequency <= 0){
+    ePow = 0;
+  }
+  #endif
+  ePow = 1-exp(-deltaTime * 2 * PI * cutoffFrequency);
+}
