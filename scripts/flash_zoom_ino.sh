fd39f35c2dcfcf42cdef74ada0e5b3b376565bef
Alan Mond <alan.mond@gmail.com>
2021-08-26 13:50:22 -0700

    Add update windows script
    Changes

--- scripts/flash_zoom_ino.sh
+++ scripts/flash_zoom_ino.sh
@@ -1,1 +1,2 @@
-avrdude -v -p m32u4 -c avr109 -P /dev/cu.usbmodem14501 -b 57600 -D -U flash:w:zoom_button.ino.hex:i
+#!/usr/bin/env bash
+avrdude -v -p m32u4 -c avr109 -P "$(ls /dev/cu.usbmodem*)" -b 57600 -D -U flash:w:$1:i
