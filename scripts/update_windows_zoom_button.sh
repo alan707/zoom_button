027c6622f5189c96983ef6fea2c51582c5b60270
Alan Mond <alan.mond@gmail.com>
2021-08-26 23:30:55 -0700

    Improve update script

--- scripts/update_windows_zoom_button.sh
+++ scripts/update_windows_zoom_button.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# check if homebrew is installed
+which -s brew
+if [[ $? != 0 ]]
+then
+    # Install Homebrew
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-brew install avrdude
-./flash_zoom_ino.sh zoom_button_windows.hex
+# check if avrdude is installed
+brew ls --versions avrdude
+if [[ $? != 0 ]]
+then
+    brew install avrdude
+fi
+
+ls /dev/cu.usbmodem*
+if [[ $? != 0 ]]
+then
+    echo "zoom button is not plugged in, please plug it into a USB port"
+elif [[ "$(ls /dev/cu.usbmodem*)" = "/dev/cu.usbmodemHIDPC1" ]]
+then
+    echo "please retry running this program immediately after plugging usb back in"
+else
+    ./flash_zoom_ino.sh zoom_button_windows.hex
+fi
+
+
